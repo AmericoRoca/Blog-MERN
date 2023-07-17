e8f04b4423782204d08f2ea91a219b2b646a1b36
Américo <americochiclana@gmail.com>
2023-07-17 17:40:49 +0200

    helper

--- api-rest/controllers/article.js
+++ api-rest/controllers/article.js
@@ -142,24 +142,7 @@ const updateArticle = async (req,res) =>{
 
   let params = req.body;
 
-    // Validating the data
-    let validarTitulo = !validator.isEmpty(params.title) && validator.isLength(params.title, { min: 1, max: 100 });
-    let validarContenido = !validator.isEmpty(params.content) && validator.isLength(params.content, { min: 1, max: 150 });
-  
-    if (!validarTitulo || !validarContenido) {
-      let errors = [];
-      if (!validarTitulo) {
-        errors.push({ field: "title", message: "El título es inválido." });
-      }
-      if (!validarContenido) {
-        errors.push({ field: "content", message: "El contenido es inválido." });
-      }
-      return res.status(400).json({
-        status: "Error",
-        mensaje: "No se ha validado la información",
-        errors,
-      });
-    }
+  validarArticulo(res,params)
 
   try {
 
@@ -183,6 +166,29 @@ const updateArticle = async (req,res) =>{
     
   } catch (error) {
     
+  }
+
+}
+
+const validarArticulo = (res, params) =>{
+
+  // Validating the data
+  let validarTitulo = !validator.isEmpty(params.title) && validator.isLength(params.title, { min: 1, max: 100 });
+  let validarContenido = !validator.isEmpty(params.content) && validator.isLength(params.content, { min: 1, max: 150 });
+
+  if (!validarTitulo || !validarContenido) {
+    let errors = [];
+    if (!validarTitulo) {
+      errors.push({ field: "title", message: "El título es inválido." });
+    }
+    if (!validarContenido) {
+      errors.push({ field: "content", message: "El contenido es inválido." });
+    }
+    return res.status(400).json({
+      status: "Error",
+      mensaje: "No se ha validado la información",
+      errors,
+    });
   }
 
 }
