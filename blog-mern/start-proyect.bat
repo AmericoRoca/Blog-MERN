8b7f1215cb60df6f719e6e1065f633cd0fee0110
Américo <americochiclana@gmail.com>
2023-07-17 19:49:10 +0200

    running program

---
+++ blog-mern/start-proyect.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:/data/db"
+start cmd.exe /k "cd C:\Users\ameri\OneDrive\Documentos\GitHub\Blog-MERN\api-rest && npm start"
+start cmd.exe /k "cd C:\Users\ameri\OneDrive\Documentos\GitHub\Blog-MERN\blog-mern && npm run dev"
+start C:\Users\ameri\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\ameri\AppData\Local\Postman\Postman.exe
