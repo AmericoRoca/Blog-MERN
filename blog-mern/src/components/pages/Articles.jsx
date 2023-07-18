da9263ecc4366378972d8983796f33e5e38b1df1
Américo <americochiclana@gmail.com>
2023-07-18 17:20:30 +0200

    article

--- blog-mern/src/components/pages/Articles.jsx
+++ blog-mern/src/components/pages/Articles.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export const Articles = () => {
   return (
-    <div>Articles</div>
-  )
-}
+    <>
+      <article className="articulo-item">
+        <div className="mask">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+        </div>
+        <div className="datos">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description"></p>
+          <button className="edit">Edit</button>
+          <button className="delete">Remove</button>
+        </div>
+      </article>
+    </>
+  );
+};
