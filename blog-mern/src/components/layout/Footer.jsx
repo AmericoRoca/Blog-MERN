99926010dbd9d033a2ff1219ddcce206735d9718
Américo <americochiclana@gmail.com>
2023-07-18 18:26:41 +0200

    getArticles from API

--- blog-mern/src/components/layout/Footer.jsx
+++ blog-mern/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer class="footer">
+    <footer className="footer">
         &copy; Created by <a>americoroca.com</a>
     </footer>
   )
--- blog-mern/src/components/pages/Articles.jsx
+++ blog-mern/src/components/pages/Articles.jsx
@@ -1,19 +1,55 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Articles = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  const getArticles = async () => {
+    const url = "http://localhost:3900/api/get-articles";
+
+    //peticion ajax
+    let peticion = await fetch(url, {
+      method: "GET",
+    });
+
+    //conseguir datos
+    let datos = await peticion.json();
+
+    //asignar datos
+    if (datos.status === "success") {
+      setArticles(datos.articulos);
+    }
+
+    console.log(datos.articulos);
+  };
+
   return (
     <>
-      <article className="articulo-item">
-        <div className="mask">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
-        </div>
-        <div className="datos">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description"></p>
-          <button className="edit">Edit</button>
-          <button className="delete">Remove</button>
-        </div>
-      </article>
+      {articles.length >= 1 ? (
+        articles.map((articulo) => {
+          return (
+
+              <article key={articulo._id} className="articulo-item">
+                <div className="mask">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+                </div>
+                <div className="datos">
+                  <h3 className="title">{articulo.title}</h3>
+                  <p className="description">{articulo.content}</p>
+                  <button className="edit">Edit</button>
+                  <button className="delete">Remove</button>
+                </div>
+              </article>
+          );
+        })
+      ) : (
+        <h1>No hay articulos</h1>
+      )}
     </>
   );
 };
