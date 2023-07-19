264c2aa74aa6f7c88a54342d4666b19214fcad82
Américo <americochiclana@gmail.com>
2023-07-19 13:01:42 +0200

    form 1.0

--- blog-mern/src/components/layout/NavBlog.jsx
+++ blog-mern/src/components/layout/NavBlog.jsx
@@ -5,9 +5,9 @@ export const NavBlog = () => {
   return (
     <nav className="nav">
     <ul>
-        <li><NavLink to="/index">Index</NavLink></li>
-        <li><NavLink to="/articles">Articles</NavLink></li>
-        <li><NavLink to="/create">Blog</NavLink></li>
+        <li><NavLink to="/index">Inicio</NavLink></li>
+        <li><NavLink to="/articles">Artículos</NavLink></li>
+        <li><NavLink to="/create">Crear Artículo</NavLink></li>
         <li><NavLink to="/contact">Contacto</NavLink></li>
     </ul>
 </nav>
--- blog-mern/src/components/pages/Create.jsx
+++ blog-mern/src/components/pages/Create.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const Create = () => {
+
+  const {formulario, enviado, cambiado} = useForm({});
+  
+  const guardarArticulo = (e) =>{
+    
+    e.preventDefault();
+    let nuevoArticulo = JSON.stringify(formulario);
+
+    console.log(nuevoArticulo);
+  }
+
+
   return (
     <div className='jumbo'>
       <h1>Crear articulo</h1>
+      <pre>{JSON.stringify(formulario)}</pre>
 
       {/* Montar formulario */}
-      <form className='formulario'>
+      <form className='formulario' onSubmit={guardarArticulo}>
 
         <div className='form-group'>
           <label htmlFor='title'>Titulo</label>
-          <input type='text' name='title'/>
+          <input type='text' name='title' onChange={cambiado}/>
         </div>
 
         <div className='form-group'>
           <label htmlFor='content'>Contenido</label>
-          <input type='textarea' name='content'/>
+          <input type='textarea' name='content' onChange={cambiado}/>
         </div>
 
         <div className='form-group'>
           <label htmlFor='file'>Imagen</label>
-          <input type='file' name='file' id='file'/>
+          <input type='file' name='file' id='file' />
         </div>
 
         <input type='submit' value="guardar" className='btn btn-success'/>
