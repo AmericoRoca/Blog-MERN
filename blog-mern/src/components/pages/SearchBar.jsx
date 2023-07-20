ac0a30cb8bce469e298e8e10d704624e7b2cd204
Américo <americochiclana@gmail.com>
2023-07-20 19:55:48 +0200

    fixing searchBar

--- blog-mern/src/components/pages/SearchBar.jsx
+++ blog-mern/src/components/pages/SearchBar.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
+import { useParams } from "react-router-dom";
 
 export const SearchBar = () => {
+
+  const [articles, setArticles] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const params = useParams();
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  useEffect(() => {
+    getArticles();
+  }, [params]);
+
+  const getArticles = async () => {
+
+    const { datos, cargando } = await Peticion(
+      Global.url + "buscar/"+params.busqueda,
+      "GET"
+    );
+
+    //asignar datos
+    if (datos.status === "success") {
+      setArticles(datos.articulos);
+    } else {
+      setArticles([])
+    }
+
+    setCargando(false);
+   
+  };
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <>
+      {cargando ? (
+        "Cargando..."
+      ) : articles.length >= 1 ? (
+        <Listado
+          articles={articles}
+          setArticles={setArticles}/>
+      ) : (
+        <h1>No hay articulos</h1>
+      )}
+    </>
+  );
+};
