22b9cc1cfbfc33aa1d1ef2316826b3f65fc7b077
Américo <americochiclana@gmail.com>
2023-07-25 12:59:51 +0200

    contact

--- blog-mern/src/components/pages/Contact.jsx
+++ blog-mern/src/components/pages/Contact.jsx
@@ -9,7 +9,7 @@ export const Contact = () => {
         <h3 className='contact-box'>Fullstack developer</h3>
         <h3 className='contact-box'>Ámsterdam</h3>
         <h3>Visit my website to get more info, feel free to contact me!</h3>
-        <a href="https://www.americoroca.com" target='blank'>www.americoroca.com</a>
+        <a href="https://www.americoroca.com" target='blank' className='link-contact'>www.americoroca.com</a>
     </div>
   )
 }
