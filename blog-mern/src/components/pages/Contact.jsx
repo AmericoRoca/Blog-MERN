f8a526cc1b9b8cc14ed8e8f3701434eaad57a51a
Américo <americochiclana@gmail.com>
2023-07-25 13:52:36 +0200

    Typo

--- blog-mern/src/components/pages/Contact.jsx
+++ blog-mern/src/components/pages/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
         <h1>Contact</h1>
         <h3 className='contact-box'>Americo Roca</h3>
         <h3 className='contact-box'>Fullstack developer</h3>
-        <h3 className='contact-box'>Ámsterdam</h3>
+        <h3 className='contact-box'>Amsterdam</h3>
         <h3>Visit my website to get more info, feel free to contact me!</h3>
         <a href="https://www.americoroca.com" target='blank' className='link-contact'>www.americoroca.com</a>
     </div>
