4a20858c701c5bba6209b901590b7ae724856268
Américo <84201638+AmericoRoca@users.noreply.github.com>
2023-07-25 10:17:44 +0200

    Update Articles.jsx

--- blog-mern/src/components/pages/Articles.jsx
+++ blog-mern/src/components/pages/Articles.jsx
@@ -43,7 +43,7 @@ export const Articles = () => {
       ) : (
         <div className="jumbo">
           <h1>There is no articles</h1>
-          <img src="src/assets/media/cry.png" alt="cry-logo" />
+          <img src="Blog-MERN/blog-mern/src/assets/media/cry.png" alt="cry-logo" />
           <h2>Go to <Link to="/create" className="link-link">Create article</Link> to add the first one</h2>
         </div>
       )}
